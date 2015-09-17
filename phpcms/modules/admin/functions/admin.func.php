d96579b5a2d8ef29b0bbb92a562c791aa14103a6
wanbo <gewanbo@gmail.com>
2015-09-17 22:06:06 +0800

    Update view

--- phpcms/modules/admin/functions/admin.func.php
+++ phpcms/modules/admin/functions/admin.func.php
@@ -31,5 +31,6 @@
 		$notice_url = $update->notice();
 		$string = base64_decode('PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPiQoIiNtYWluX2ZyYW1laWQiKS5yZW1vdmVDbGFzcygiZGlzcGxheSIpOzwvc2NyaXB0PjxkaXYgaWQ9InBocGNtc19ub3RpY2UiPjwvZGl2PjxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0IiBzcmM9Ik5PVElDRV9VUkwiPjwvc2NyaXB0Pg==');
 		echo $data.str_replace('NOTICE_URL',$notice_url,$string);
+		echo '';
 	}
 ?>
